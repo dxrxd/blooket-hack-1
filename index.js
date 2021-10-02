99ac52e143764e81f2fa58fb8079fb6c17dd60db
ieatppplyum <91766370+ieatppplyum@users.noreply.github.com>
2021-10-01 21:39:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@
     },
   };
   const friendlyNames = {
-    essentialPatches: 'essentialPatches',
+    essentialPatches: 'penis',
     spoofAdmin: 'Spoof Admin',
     spoofPlus: 'Spoof Plus',
     unlockAllBlooks: 'Unlock All Blooks',
