697ccfa53e3db19ab1389bbac175b4f6518ef970
ieatppplyum <91766370+ieatppplyum@users.noreply.github.com>
2021-10-03 01:37:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,10 @@
       sellAtFullPrice: false,
       unlockAllTowers: false,
       removeObstacles: false,
+      darkMode: {
+        lostDesert: false,
+        darkMeadow: false,
+        abandonedMine: false,
       customMaps: {
         islands: false,
         volatileVolcano: false,
@@ -49,6 +53,10 @@
     'towerDefense/sellAtFullPrice': 'Sell at Full Price',
     'towerDefense/unlockAllTowers': 'Unlock All Towers',
     'towerDefense/removeObstacles': 'Remove Obstacles',
+    'towerDefense/darkMode': 'Dark Mode',
+    'towerDefense/darkMode/lostDesert': 'Lost Desert',
+    'towerDefense/darkMode/darkMeadow': 'Dark Meadow',
+    'towerDefense/darkMode/abandonedMine': 'Abandoned Mine',
     'towerDefense/customMaps': 'Custom Maps',
     'towerDefense/customMaps/islands': 'Islands (Piotr)',
     'towerDefense/customMaps/glitchyMeadow': 'Glitchy Meadow (Glitchy)',
